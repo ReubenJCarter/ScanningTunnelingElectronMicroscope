e007697016179c9abed934fdb691b2aed850f372
Reuben Carter <reubenjcarter@gmail.com>
2018-02-24 17:25:21 +0000

    added adc driver code

---
+++ Software/Firmware/Firmware/src/LTC2326Driver.c
@@ -0,0 +1,78 @@
+/*
+ * LTC2326Driver.c
+ *
+ * Created: 24/02/2018 15:37:00
+ *  Author: reuben
+ */ 
+
+#include "LTC2326Driver.h"
+
+#include "asf.h"
+
+
+void InitADC(void)
+{
+	//set all the pin modes
+	ioport_set_pin_dir(CNV_ADCPIN, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(BUSY_ADCPIN, IOPORT_DIR_INPUT);
+	ioport_set_pin_dir(SCK_ADCPIN, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(SDO_ADCPIN, IOPORT_DIR_INPUT);
+	ioport_set_pin_dir(CHAIN_ADCPIN, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(RDLSDI_ADCPIN, IOPORT_DIR_OUTPUT);
+	
+	//make CHAIN_ADCPIN set to 0 at start up to set the device to normal mode
+	ioport_set_pin_level(CHAIN_ADCPIN, 0);
+	
+	//make RDLSDI_ADCPIN set to 0 at start up to enable thew data
+	ioport_set_pin_level(RDLSDI_ADCPIN, 0);
+	
+	//set cnv to low to start with
+	ioport_set_pin_level(CNV_ADCPIN, 0);
+	
+	//sck to low to start
+	ioport_set_pin_level(SCK_ADCPIN, 0);
+	
+}
+
+uint16_t ReadADC(void)
+{
+	
+	//set cnv (convert) signal to high to start a conversion
+	ioport_set_pin_level(CNV_ADCPIN, 1);
+	
+	//wait some time
+	delay_us(10);
+	
+	//wait for Busy to be low 
+	while(ioport_get_pin_level(BUSY_ADCPIN)); 
+	
+	//wait some time
+	delay_us(10);
+	
+	//reset convert signal
+	ioport_set_pin_level(CNV_ADCPIN, 1);
+	
+	//read each bit
+	uint16_t finalData = 0; 
+	for(int i = 0; i < 16; i++)
+	{
+		//read data 
+		int dataBit = ioport_get_pin_level(SDO_ADCPIN);
+		
+		finalData = (finalData << 1) | (dataBit & 0x1);
+		
+		//wait a bit
+		delay_us(10);
+		
+		//clk next data value
+		ioport_set_pin_level(SCK_ADCPIN, 1);
+		
+		//wait a bit
+		delay_us(10);
+		
+		//clk low
+		ioport_set_pin_level(SCK_ADCPIN, 1);
+	}
+	
+	return finalData;
+}
---
+++ Software/Firmware/Firmware/src/LTC2326Driver.h
@@ -0,0 +1,28 @@
+/*
+ * LTC2326Driver.h
+ *
+ * Created: 24/02/2018 15:36:38
+ *  Author: reuben
+ */ 
+
+
+#ifndef LTC2326DRIVER_H_
+#define LTC2326DRIVER_H_
+
+#include <stdint.h>
+#include <asf.h>
+
+
+#define CNV_ADCPIN		PIO_PD26_IDX//input rising edge starts a new conversion
+#define BUSY_ADCPIN		PIO_PC31_IDX//output goes high at start of new convesion, returns low when finished 
+#define SCK_ADCPIN		PIO_PD30_IDX//input serial data clock input, data shifted out on the rising edge of clk
+#define SDO_ADCPIN		PIO_PA19_IDX//output serial data output (2's complement format)
+#define CHAIN_ADCPIN	PIO_PA19_IDX//input chain mode selector. when low, operates in normal mode
+#define RDLSDI_ADCPIN	PIO_PA19_IDX//input whn chain is low the is a bus enable input, else its a data input
+
+
+void InitADC(void);
+uint16_t ReadADC(void);
+
+
+#endif /* LTC2326DRIVER_H_ */
